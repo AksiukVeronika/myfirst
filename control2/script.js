d4aa9c01f23d37a0256c2accbaa0885ffdd4809e
AksiukVeronika <159823203+AksiukVeronika@users.noreply.github.com>
2024-04-09 12:58:52 +0300

    Update script.js

--- control2/script.js
+++ control2/script.js
@@ -1,1 +1,19 @@
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+let array = [];
+for (let i = 0; i < 20; i++) {
+    array.push(generateRandomNumber());
+}
+
+console.log("Початковий масив:");
+console.log(array);
+
+array.sort(function(a, b) {
+    return a - b;
+});
+
+console.log("Відредагований масив за зростанням:");
+console.log(array);
 
