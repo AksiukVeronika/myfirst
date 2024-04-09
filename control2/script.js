5e1d261539d045f0713482a603e9ae07b45d2701
AksiukVeronika <159823203+AksiukVeronika@users.noreply.github.com>
2024-04-09 13:43:07 +0300

    Update script.js

--- control2/script.js
+++ control2/script.js
@@ -1,35 +1,65 @@
-window.onload = function() {
-    const heightSelect = document.getElementById("heightSelect");
-    const generateButton = document.getElementById("generateButton");
-    const generatedDiv = document.getElementById("generatedDiv");
-
-    function generateDiv() {
-        const selectedHeight = heightSelect.value + "px";
-        const generatedHTML = <div id="generatedBlock" style="background-color: blue; width: 30px; height: ${selectedHeight}; position: relative;"></div>;
-        generatedDiv.innerHTML = generatedHTML;
-
-        const moveLeftButton = document.createElement("button");
-        moveLeftButton.textContent = "Посунути вліво";
-        moveLeftButton.addEventListener("click", function() {
-            moveBlock(-20);
-        });
-
-        const moveRightButton = document.createElement("button");
-        moveRightButton.textContent = "Посунути вправо";
-        moveRightButton.addEventListener("click", function() {
-            moveBlock(20);
-        });
-
-        generatedDiv.appendChild(moveLeftButton);
-        generatedDiv.appendChild(moveRightButton);
+(function (global) {
+    function getRandomInt(max = 100) {
+        return Math.floor(Math.random() * max);
     }
 
-    generateButton.addEventListener("click", generateDiv);
-
-    function moveBlock(offset) {
-        const generatedBlock = document.getElementById("generatedBlock");
-        const currentLeft = parseInt(window.getComputedStyle(generatedBlock).left || 0);
-        generatedBlock.style.left = (currentLeft + offset) + "px";
+    function generateArray(n) {
+        let array = [];
+    for (let index = 0; index < n; index++) {
+        array[index] = getRandomInt();
+        }
+        return array;
     }
-    generateDiv();
-};
+
+    let array = generateArray(20);
+     console.log(SortLibrary.studentyEvakyjytsaPidChasMinuvannyaSort(array, true));
+        document.getElementById("container").textContent = array;
+           document.getElementById("generate-button").addEventListener("click", (event) => {
+        console.log();
+        if (
+            !document.getElementById("select-container") &&
+              document.getElementById("height").value
+        ) {
+            createContainer();
+        } else {
+            document.getElementById("select-container").remove();
+              document.getElementById("left").remove();
+                document.getElementById("right").remove();
+            createContainer();
+        }
+    });
+    function createContainer() {
+        const div = document.createElement("div");
+         div.id = "select-container";
+          div.style.backgroundColor = "blue";
+           div.style.position = "relative";
+            div.style.width = "30px";
+           div.style.height = ${document.getElementById("height").value}px;
+          document.body.appendChild(div);
+        createButtons();
+    }
+    function createButtons() {
+        const buttonLeft = document.createElement("button");
+         const buttonRight = document.createElement("button");
+          buttonLeft.id = "left";
+         buttonRight.id = "right";
+        buttonLeft.textContent = "left";
+        buttonRight.textContent = "right";
+
+        buttonLeft.addEventListener("click", (event) => {
+            let leftVal = document.getElementById("select-container").style.left;
+              leftVal = leftVal.replace("px", "");
+              leftVal = Number(leftVal) - 20;
+            document.getElementById("select-container").style.left = ${leftVal}px;
+        });
+        buttonRight.addEventListener("click", (event) => {
+            let rightVal = document.getElementById("select-container").style.left;
+              rightVal = rightVal.replace("px", "");
+               rightVal = Number(rightVal) + 20;
+            document.getElementById("select-container").style.left = ${rightVal}px;
+        });
+
+        document.body.appendChild(buttonLeft);
+        document.body.appendChild(buttonRight);
+    }
+})(window);
