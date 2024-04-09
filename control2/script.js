38a0c116c5c8f153e7da4757e334581765db42e9
AksiukVeronika <159823203+AksiukVeronika@users.noreply.github.com>
2024-04-09 13:24:39 +0300

    Update script.js

--- control2/script.js
+++ control2/script.js
@@ -1,16 +1,13 @@
 window.onload = function() {
-    // Вибір елементів з DOM
     const heightSelect = document.getElementById("heightSelect");
     const generateButton = document.getElementById("generateButton");
     const generatedDiv = document.getElementById("generatedDiv");
 
-    // Функція для генерації елементу div з характеристиками
     function generateDiv() {
         const selectedHeight = heightSelect.value + "px";
         const generatedHTML = <div id="generatedBlock" style="background-color: blue; width: 30px; height: ${selectedHeight}; position: relative;"></div>;
         generatedDiv.innerHTML = generatedHTML;
 
-        // Додаємо кнопки "Посунути вліво" і "Посунути вправо"
         const moveLeftButton = document.createElement("button");
         moveLeftButton.textContent = "Посунути вліво";
         moveLeftButton.addEventListener("click", function() {
@@ -27,16 +24,12 @@ window.onload = function() {
         generatedDiv.appendChild(moveRightButton);
     }
 
-    // Додати обробник подій на кнопку "Згенерувати"
     generateButton.addEventListener("click", generateDiv);
 
-    // Функція для переміщення блоку
     function moveBlock(offset) {
         const generatedBlock = document.getElementById("generatedBlock");
         const currentLeft = parseInt(window.getComputedStyle(generatedBlock).left || 0);
         generatedBlock.style.left = (currentLeft + offset) + "px";
     }
-
-    // Викликати функцію generateDiv() для відображення елементу div зі значенням, вибраним за замовчуванням
     generateDiv();
 };
